7ffd118980b42480b3ac413f4543964e43a7d1d2
lavdim <lavdimhalilaj@yahoo.com>
2015-08-13 09:10:09 +0200

    keod

---
+++ keod/internals.pl
@@ -0,0 +1,190 @@
+# LaTeX2HTML 2006-1 (1.71)
+# Associate internals original text with physical files.
+
+
+$key = q/cite_manaf2012current/;
+$ref_files{$key} = "$dir".q|node53.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_baclawski2002consistency/;
+$ref_files{$key} = "$dir".q|node53.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_abburu2012survey/;
+$ref_files{$key} = "$dir".q|node53.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_svatek2010entity/;
+$ref_files{$key} = "$dir".q|node53.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_suarez2010neon/;
+$ref_files{$key} = "$dir".q|node53.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_bizer2009linked/;
+$ref_files{$key} = "$dir".q|node53.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_kalliamvakou2015open/;
+$ref_files{$key} = "$dir".q|node53.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_schober2007towards/;
+$ref_files{$key} = "$dir".q|node53.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_svatek2009ontology/;
+$ref_files{$key} = "$dir".q|node53.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/subsec:Dereferenceability/;
+$ref_files{$key} = "$dir".q|node27.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:vocab-analysis/;
+$ref_files{$key} = "$dir".q|node3.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_sirin2006wine/;
+$ref_files{$key} = "$dir".q|node53.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_schober2009survey/;
+$ref_files{$key} = "$dir".q|node53.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_berners2000weaving/;
+$ref_files{$key} = "$dir".q|node53.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/tab:reuse/;
+$ref_files{$key} = "$dir".q|node12.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_baker2013key/;
+$ref_files{$key} = "$dir".q|node53.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:phases/;
+$ref_files{$key} = "$dir".q|node4.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_d2008neon/;
+$ref_files{$key} = "$dir".q|node53.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:objectPropRange/;
+$ref_files{$key} = "$dir".q|node3.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_schober2012ontocheck/;
+$ref_files{$key} = "$dir".q|node53.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:objectPropDomain/;
+$ref_files{$key} = "$dir".q|node3.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_gomez2006ontological/;
+$ref_files{$key} = "$dir".q|node53.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_pinto2004diligent/;
+$ref_files{$key} = "$dir".q|node53.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:best-practices/;
+$ref_files{$key} = "$dir".q|node4.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_heath2011linked/;
+$ref_files{$key} = "$dir".q|node53.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/tab:vocabs/;
+$ref_files{$key} = "$dir".q|node3.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_poveda2012reuse/;
+$ref_files{$key} = "$dir".q|node53.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_peroni2013tools/;
+$ref_files{$key} = "$dir".q|node53.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_dodds2011linked/;
+$ref_files{$key} = "$dir".q|node53.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:conclusion/;
+$ref_files{$key} = "$dir".q|node52.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_di2011just/;
+$ref_files{$key} = "$dir".q|node53.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_schlicht2006h/;
+$ref_files{$key} = "$dir".q|node53.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:bottom-up/;
+$ref_files{$key} = "$dir".q|node2.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_kezadri2010first/;
+$ref_files{$key} = "$dir".q|node53.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_gracia2012challenges/;
+$ref_files{$key} = "$dir".q|node53.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_poveda2012validating/;
+$ref_files{$key} = "$dir".q|node53.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_zeginis2013collaborative/;
+$ref_files{$key} = "$dir".q|node53.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_auer2007rapidowl/;
+$ref_files{$key} = "$dir".q|node53.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/p:skos-altLabel/;
+$ref_files{$key} = "$dir".q|node50.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_yu2009requirements/;
+$ref_files{$key} = "$dir".q|node53.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_montiel2011style/;
+$ref_files{$key} = "$dir".q|node53.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_jansen2011ten/;
+$ref_files{$key} = "$dir".q|node53.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_haslhofer2013using/;
+$ref_files{$key} = "$dir".q|node53.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_pedrinaci2014linked/;
+$ref_files{$key} = "$dir".q|node53.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_suarez2012neon/;
+$ref_files{$key} = "$dir".q|node53.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:related-work/;
+$ref_files{$key} = "$dir".q|node51.html|; 
+$noresave{$key} = "$nosave";
+
+1;
+
---
+++ keod/labels.pl
@@ -0,0 +1,249 @@
+# LaTeX2HTML 2006-1 (1.71)
+# Associate labels original text with physical files.
+
+
+$key = q/cite_montiel2011style/;
+$external_labels{$key} = "$URL/" . q|node53.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_yu2009requirements/;
+$external_labels{$key} = "$URL/" . q|node53.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_auer2007rapidowl/;
+$external_labels{$key} = "$URL/" . q|node53.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/p:skos-altLabel/;
+$external_labels{$key} = "$URL/" . q|node50.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_zeginis2013collaborative/;
+$external_labels{$key} = "$URL/" . q|node53.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_poveda2012validating/;
+$external_labels{$key} = "$URL/" . q|node53.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_gracia2012challenges/;
+$external_labels{$key} = "$URL/" . q|node53.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_kezadri2010first/;
+$external_labels{$key} = "$URL/" . q|node53.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_pedrinaci2014linked/;
+$external_labels{$key} = "$URL/" . q|node53.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_suarez2012neon/;
+$external_labels{$key} = "$URL/" . q|node53.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:related-work/;
+$external_labels{$key} = "$URL/" . q|node51.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_haslhofer2013using/;
+$external_labels{$key} = "$URL/" . q|node53.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_jansen2011ten/;
+$external_labels{$key} = "$URL/" . q|node53.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_peroni2013tools/;
+$external_labels{$key} = "$URL/" . q|node53.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/tab:vocabs/;
+$external_labels{$key} = "$URL/" . q|node3.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_poveda2012reuse/;
+$external_labels{$key} = "$URL/" . q|node53.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_heath2011linked/;
+$external_labels{$key} = "$URL/" . q|node53.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:best-practices/;
+$external_labels{$key} = "$URL/" . q|node4.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_pinto2004diligent/;
+$external_labels{$key} = "$URL/" . q|node53.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_gomez2006ontological/;
+$external_labels{$key} = "$URL/" . q|node53.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:bottom-up/;
+$external_labels{$key} = "$URL/" . q|node2.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_schlicht2006h/;
+$external_labels{$key} = "$URL/" . q|node53.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_di2011just/;
+$external_labels{$key} = "$URL/" . q|node53.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:conclusion/;
+$external_labels{$key} = "$URL/" . q|node52.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_dodds2011linked/;
+$external_labels{$key} = "$URL/" . q|node53.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_schober2012ontocheck/;
+$external_labels{$key} = "$URL/" . q|node53.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:objectPropRange/;
+$external_labels{$key} = "$URL/" . q|node3.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_d2008neon/;
+$external_labels{$key} = "$URL/" . q|node53.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_baker2013key/;
+$external_labels{$key} = "$URL/" . q|node53.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:phases/;
+$external_labels{$key} = "$URL/" . q|node4.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/tab:reuse/;
+$external_labels{$key} = "$URL/" . q|node12.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_berners2000weaving/;
+$external_labels{$key} = "$URL/" . q|node53.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_schober2009survey/;
+$external_labels{$key} = "$URL/" . q|node53.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_sirin2006wine/;
+$external_labels{$key} = "$URL/" . q|node53.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:vocab-analysis/;
+$external_labels{$key} = "$URL/" . q|node3.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:objectPropDomain/;
+$external_labels{$key} = "$URL/" . q|node3.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_abburu2012survey/;
+$external_labels{$key} = "$URL/" . q|node53.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_baclawski2002consistency/;
+$external_labels{$key} = "$URL/" . q|node53.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_manaf2012current/;
+$external_labels{$key} = "$URL/" . q|node53.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/subsec:Dereferenceability/;
+$external_labels{$key} = "$URL/" . q|node27.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_svatek2009ontology/;
+$external_labels{$key} = "$URL/" . q|node53.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_schober2007towards/;
+$external_labels{$key} = "$URL/" . q|node53.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_bizer2009linked/;
+$external_labels{$key} = "$URL/" . q|node53.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_kalliamvakou2015open/;
+$external_labels{$key} = "$URL/" . q|node53.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_suarez2010neon/;
+$external_labels{$key} = "$URL/" . q|node53.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_svatek2010entity/;
+$external_labels{$key} = "$URL/" . q|node53.html|; 
+$noresave{$key} = "$nosave";
+
+1;
+
+
+# LaTeX2HTML 2006-1 (1.71)
+# labels from external_latex_labels array.
+
+
+$key = q/sec:best-practices/;
+$external_latex_labels{$key} = q|4|; 
+$noresave{$key} = "$nosave";
+
+$key = q/tab:reuse/;
+$external_latex_labels{$key} = q|2|; 
+$noresave{$key} = "$nosave";
+
+$key = q/p:skos-altLabel/;
+$external_latex_labels{$key} = q|4.7.1|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:vocab-analysis/;
+$external_latex_labels{$key} = q|3|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:objectPropRange/;
+$external_latex_labels{$key} = q|2|; 
+$noresave{$key} = "$nosave";
+
+$key = q/_/;
+$external_latex_labels{$key} = q|<|; 
+$noresave{$key} = "$nosave";
+
+$key = q/tab:vocabs/;
+$external_latex_labels{$key} = q|1|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:phases/;
+$external_latex_labels{$key} = q|3|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:conclusion/;
+$external_latex_labels{$key} = q|6|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:objectPropDomain/;
+$external_latex_labels{$key} = q|1|; 
+$noresave{$key} = "$nosave";
+
+$key = q/subsec:Dereferenceability/;
+$external_latex_labels{$key} = q|4.3.1|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:bottom-up/;
+$external_latex_labels{$key} = q|2|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:related-work/;
+$external_latex_labels{$key} = q|5|; 
+$noresave{$key} = "$nosave";
+
+1;
+
